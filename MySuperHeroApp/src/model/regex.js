8030ae7778b829457191ab16621047b9878d99d5
wassimouricha <wbouricha5@gmail.com>
2022-12-23 15:03:26 +0100

    new regex for ts superhero

---
+++ MySuperHeroApp/src/model/regex.js
@@ -0,0 +1,12 @@
+var expression = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+var email = 'wbouricha5@gmail.com';
+var result = expression.test(email); // true
+console.log('l\'e-mail est ' + (result ? 'correct' : 'incorrect'));
+var regexName = new RegExp(/^[a-zA-Z0-9 ]*$/);
+var nameR = "wassim";
+var resultName = regexName.test(nameR);
+console.log('Le nom est ' + (resultName ? 'correct' : 'incorrect'));
+var regexPower = /^[a-zA-Z]*$/;
+var power = "spidersense";
+var resultPower = regexPower.test(power);
+console.log('Le pouvoir est ' + (resultPower ? 'correct' : 'incorrect'));
---
+++ MySuperHeroApp/src/model/regex.ts
@@ -0,0 +1,18 @@
+const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+const email: string = 'wbouricha5@gmail.com';
+const result: boolean = expression.test(email); // true
+
+console.log('l\'e-mail est ' + (result ? 'correct' : 'incorrect'));
+
+const regexName = new RegExp(/^[a-zA-Z0-9 ]*$/) ;
+const nameR: string = "wassim";
+const resultName: boolean = regexName.test(nameR);
+
+console.log('Le nom est ' + (resultName ? 'correct' : 'incorrect'));
+
+const regexPower: RegExp = /^[a-zA-Z]*$/;
+const power: string = "spidersense";
+const resultPower: boolean = regexPower.test(power);
+
+console.log('Le pouvoir est ' + (resultPower ? 'correct' : 'incorrect'));
